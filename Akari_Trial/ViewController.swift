95d1a5e967865d5c36e8b9f97c29e1db924f64f1
hiro-160 <c011633262@edu.teu.ac.jp>
2018-08-26 22:25:13 +0900

    Initial Commit

---
+++ Akari_Trial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Akari_Trial
+//
+//  Created by Hirohito on 2018/08/26.
+//  Copyright © 2018年 Hirohito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
